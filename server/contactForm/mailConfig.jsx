44332986c486a3f8a90dbeae411f92867315cb1b
patrickmennig <patrick.mennig@googlemail.com>
2016-11-06 00:27:18 +0100

    added mailConfig with default data for clarity, removed unnecessary require of css file

---
+++ server/contactForm/mailConfig.jsx
@@ -0,0 +1,7 @@
+export default {
+    user:    	"username",
+    email: 	    "username/email-address",	// if user is not an email address
+    password:	"password",
+    host:    	"smtp.gmail.com",			// or others
+    ssl:     	true
+}
